2eb3ed96adc180d63950f2e84e84a10f7bc4d751
devnabibia <nangendodennis@gmail.com>
2024-01-20 20:47:37 +0300

    d

---
+++ src/Components/Footer.js
@@ -0,0 +1,110 @@
+import React from "react";
+import "../App.css";
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="footerpro1">
+          <div className="bannerpro1">
+            <br />
+            <hr id="tpro1" />
+            <div className="contentpro1">
+              <div className="left6 boxpro1">
+                <div className="upperpro1">
+                  <div className="topicpro1">About us</div>
+                  <p>
+                    At Devnabibia., we understand that change is not always easy.
+                    Since 2018, we’ve been helping individuals of various
+                    classes full their economic and financial needs. Our few
+                    years of experience have taught us to always make your
+                    personal success our priority.
+                  </p>
+                </div>
+              </div>
+              <div className="middle6 boxpro1">
+                <div className="topicpro1">Our Services</div>
+                <div>
+                  <a href="./">Home</a>
+                </div>
+                <div>
+                  <a href="./about">Why Choose DC Ikigai</a>
+                </div>
+                <div>
+                  <a href="/Subscribe">Pricing</a>
+                </div>
+                <div>
+                  <a href="./policy">Privacy Policy</a>
+                </div>
+                <div>
+                  <a href="./careers">Careers</a>
+                </div>
+              </div>
+              <div className="right6 boxpro1">
+                <div className="lowerpro1">
+                  <div className="topicpro1">Contact us</div>
+                  <div className="addresspro1">
+                    <a href="#">
+                      A/4, 42, Nairobi, Kenya,
+                      <br />
+                      Thika Road, Opp. Kenyatta Hospital,
+                      <br />
+                      Juja, kenya,
+                      <br />
+                      Nairobi- 421201
+                    </a>
+                    <br />
+                    <br />
+                  </div>
+                  <div className="phonepro1">
+                    <a href="tel:254742692485">+254742692485</a>
+                  </div>
+                </div>
+              </div>
+              <div className="iapiou65">
+                <hr />
+                <div className="bottompro1">
+                  <p>
+                    Copyright © 2024{" "}
+                    <a href="https://www.devnabibia.tech/" target="_blank">
+                      Devnabibia
+                    </a>{" "}
+                    All rights reserved
+                  </p>
+                </div>
+                <div className="media-iconspro1">
+                  <a
+                    href="#/"
+                    target="_blank"
+                  >
+                    <i className="fab fa-facebook-f"></i>
+                  </a>
+                  <a
+                    href="#"
+                    target="_blank"
+                  >
+                    <i className="fab fa-instagram"></i>
+                  </a>
+                  <a href="#">
+                    <i className="fab fa-twitter"></i>
+                  </a>
+                  <a href="#">
+                    <i className="fab fa-youtube"></i>
+                  </a>
+                  <a
+                    href="#"
+                    target="_blank"
+                  >
+                    <i className="fab fa-linkedin-in"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
